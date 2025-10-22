de5b479059bd98d0fe6c8a041501af18b92fb4e1
Brayden Franklin <S4051718@student.rmit.edu.au>
2025-10-22 11:56:16 +0000

    Added adventureStart method from gameNavigation to here. Tidied up some output text.

--- src/main/game.java
+++ src/main/game.java
@@ -8,16 +8,16 @@ public class game {
         private static Scanner scanner = new Scanner(System.in);       
         public static boolean gameRunning = true;
 
-        public static gameCharacters pCharacter = new gameCharacters(null, null, 40, 1, null, 0, 0, 0);
+        public static gameCharacters pCharacter = new gameCharacters(null, null, 10, 1, null, 0, 0, 0);
 
     public static void main(String[] args) {
         System.out.println("Welcome to the World of X\n");
         charCreate();
-        gameNavigation.adventureStart();
+        adventureStart();
         do {
             gameNavigation.navigation();
         } while (gameRunning);
-        System.out.println("--- GAME OVER ---");
+        System.out.println("\n--- GAME OVER ---");
     }
 
     public static void charCreate() {
@@ -29,22 +29,45 @@ public class game {
         
         while (!classEntered.equals("W") && !classEntered.equals("M") && !classEntered.equals("R")) {
                 classEntered = scanner.nextLine().toUpperCase();
-        switch (classEntered) {
-            case "W":
-                pCharacter.setCharType("Warrior");
-                break;
-            case "M":
-                pCharacter.setCharType("Mage");            
-                break;
-            case "R":
-                pCharacter.setCharType("Rogue");            
-                break;
-            default:
-                System.out.println("That is not a valid Character Class. Please enter W/w, M/m, or R/r");
-                break;
+            switch (classEntered) {
+                case "W":
+                    pCharacter.setCharType("Warrior");
+                    break;
+                case "M":
+                    pCharacter.setCharType("Mage");            
+                    break;
+                case "R":
+                    pCharacter.setCharType("Rogue");            
+                    break;
+                default:
+                    System.out.println("That is not a valid Character Class. Please enter W/w, M/m, or R/r");
+                    break;
+            }
         }
-    }
         System.out.println(pCharacter);        
-
     }
+
+    public static void adventureStart() {
+            System.out.println("\nYou awaken in your house in The Village upon the bottom of the Misty Mountains");   
+            System.out.println("Today is the day you leave for your quest. It's what you've been preparing your whole life for.");
+            System.out.println("The evil Lich King in the Ice Castle atop the Misty Mountains has cast his evil shadow over the land for too long.");  
+            if (game.pCharacter.getCharType().equals("Warrior")) {
+                System.out.println("But before you begin your quest you must speak to the Blacksmith in his Forge.");
+                game.pCharacter.setWeapon("Rusty Sword");
+                System.out.println("He gives you a " + game.pCharacter.getWeapon());
+                         
+            }
+            else if (game.pCharacter.getCharType().equals("Mage")) {
+                System.out.println("But before you begin your quest you must speak to the Sorceror in his Tower.");            
+                game.pCharacter.setWeapon("Short Staff");      
+                System.out.println("He gives you the " + game.pCharacter.getWeapon());                         
+            }
+            else if (game.pCharacter.getCharType().equals("Rogue")) {
+                System.out.println("But before you begin your quest you must speak to the Guildmaster in the Thieves Guild.");            
+                game.pCharacter.setWeapon("Dull Dagger");
+                System.out.println("He gives you a " + game.pCharacter.getWeapon());                                     
+            } 
+            game.pCharacter.setWeaponDamage();   
+        }    
+    
 }
