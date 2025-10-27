b298b43742576172e294b4f13fefd518a45c5558
Brayden Franklin <S4051718@student.rmit.edu.au>
2025-10-27 14:28:28 +0000

    GameCombat now uses the HealthPotion methods from Inventory. Wrapped the main battle logic in a try and catch for exception handling. Renamed to match Java best practices.

---
+++ src/combat/GameCombat.java
@@ -0,0 +1,237 @@
+package combat;
+
+import java.util.Scanner;
+import java.util.Random;
+
+import main.Game;
+import characters.GameCharacters;
+
+public class GameCombat {
+    private static Scanner scanner = new Scanner(System.in);
+    public static Random diceRoll = new Random();
+    public static boolean fightWon;
+
+    public static void combatMethod(GameCharacters player, GameCharacters enemy) {
+        
+        int tempPlayerHealth, tempEnemyHealth;
+        int playerAttackRoll = 0;
+        int enemyAttackRoll = 0;
+        boolean flee = false;
+        boolean fightOn = true;
+
+        while (fightOn) {
+
+            try {
+                System.out.println("\nWhat would you like to do?: ");
+                System.out.println("(1). Fight\n(2). Run ");
+                int potionCount = items.Inventory.getHealthPotionCount();
+                if (potionCount > 0) {
+                    System.out.printf("(3). Use Potion (%d left)\n", potionCount);
+                }                
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("-------------Combat-------------");
+                        System.out.printf("\nYou attempt to hit %s", enemy.getName());
+                        if (player.getLevel() == 0) {
+                            playerAttackRoll = diceRoll.nextInt(2) + player.getWeaponDamage();
+                        } else if (player.getLevel() == 1) {
+                            playerAttackRoll = diceRoll.nextInt(3) + player.getWeaponDamage();
+                        } else if (player.getLevel() == 2) {
+                            playerAttackRoll = diceRoll.nextInt(4) + player.getWeaponDamage();
+                        } else if (player.getLevel() == 3) {
+                            playerAttackRoll = diceRoll.nextInt(5) + player.getWeaponDamage();
+                        } else if (player.getLevel() == 4) {
+                            playerAttackRoll = diceRoll.nextInt(6) + player.getWeaponDamage();
+                        } else if (player.getLevel() == 5) {
+                            playerAttackRoll = diceRoll.nextInt(7) + player.getWeaponDamage();
+                        }
+                        if (playerAttackRoll <= 0) {
+                            System.out.printf("\nYou failed to hit %s\n", enemy.getName());
+                        } else {
+                            System.out.printf("\nYou hit %s for %d\n", enemy.getName(), playerAttackRoll);
+                            tempEnemyHealth = enemy.getCurrentHealth() - playerAttackRoll;
+                            enemy.setCurrentHealth(tempEnemyHealth);
+                        }
+                        if (enemy.getCurrentHealth() > 0) {
+                            System.out.printf("\nThe %s attempts to hit you", enemy.getName());
+                            if (enemy.getLevel() == 1) {
+                                enemyAttackRoll = diceRoll.nextInt(2);
+                            } else if (enemy.getLevel() == 2) {
+                                enemyAttackRoll = diceRoll.nextInt(3);
+                            } else if (enemy.getLevel() == 3) {
+                                enemyAttackRoll = diceRoll.nextInt(4);
+                            } else if (enemy.getLevel() == 4) {
+                                enemyAttackRoll = diceRoll.nextInt(5);
+                            } else if (enemy.getLevel() == 5) {
+                                enemyAttackRoll = diceRoll.nextInt(5);
+                            }
+                            if (enemyAttackRoll <= 0) {
+                                System.out.printf("\nThe %s failed to hit you\n", enemy.getName());
+                            } else {
+                                System.out.printf("\nThe %s hit you for %d\n", enemy.getName(), enemyAttackRoll);
+                                tempPlayerHealth = player.getCurrentHealth() - enemyAttackRoll;
+                                player.setCurrentHealth(tempPlayerHealth);
+                            }
+                            System.out.printf("\nYou have %d health remaining", player.getCurrentHealth());
+                            System.out.printf("\nThe %s has %d heatlh remaining", enemy.getName(), enemy.getCurrentHealth());
+                            System.out.println("\n--------------------------------");
+                        }
+                        break;
+                    case 2:
+                        flee = diceRoll.nextBoolean();
+                        if (flee) {
+                            System.out.println("You flee the battle");
+                            fightOn = false;
+                            Game.gameRunning = true;
+                        } else {
+                            System.out.println("You failed to flee...");
+                            System.out.printf("\nThe %s attempts to hit you\n", enemy.getName());
+                            enemyAttackRoll = diceRoll.nextInt(6);
+                            if (enemyAttackRoll <= 0) {
+                                System.out.printf("\nThe %s failed to hit you\n", enemy.getName());
+                            } else {
+                                System.out.printf("\nYou hit %s for %d\n", enemy.getName(), enemyAttackRoll);
+                                tempPlayerHealth = player.getCurrentHealth() - enemyAttackRoll;
+                                player.setCurrentHealth(tempPlayerHealth);
+                            }
+                            System.out.printf("\nYou have %d health remaining\n", player.getCurrentHealth());
+                        }
+                        break;
+                    case 3:
+                        if (items.Inventory.getHealthPotionCount() > 0) {       
+                            items.Inventory.useHealthPotion();          
+                        } else {                                               
+                        System.out.printf("\nThe %s attempts to hit you\n", enemy.getName());
+                            if (enemy.getCurrentHealth() > 0) {
+                                System.out.printf("\nThe %s attempts to hit you\n", enemy.getName());
+                                if (enemy.getLevel() == 1) {
+                                    enemyAttackRoll = diceRoll.nextInt(2);
+                                } else if (enemy.getLevel() == 2) {
+                                    enemyAttackRoll = diceRoll.nextInt(3);
+                                } else if (enemy.getLevel() == 3) {
+                                    enemyAttackRoll = diceRoll.nextInt(4);
+                                } else if (enemy.getLevel() == 4) {
+                                    enemyAttackRoll = diceRoll.nextInt(5);
+                                } else if (enemy.getLevel() == 5) {
+                                    enemyAttackRoll = diceRoll.nextInt(5);
+                                }
+                                if (enemyAttackRoll <= 0) {
+                                    System.out.printf("\nThe %s failed to hit you\n", enemy.getName());
+                                } else {
+                                    System.out.printf("\nThe %s hit you for %d\n", enemy.getName(), enemyAttackRoll);
+                                    tempPlayerHealth = player.getCurrentHealth() - enemyAttackRoll;
+                                    player.setCurrentHealth(tempPlayerHealth);
+                                }     
+                            }             
+                        }     
+                        break;
+                    default:
+                        System.out.println("Not a valid choice");
+                        break;
+                }
+                if (player.getCurrentHealth() <= 0) {
+                    fightOn = false;
+                    Game.gameRunning = false;
+                    System.out.printf("\nYou died");
+                    break;
+                }
+                if (enemy.getCurrentHealth() <= 0) {
+                    fightOn = false;
+                    fightWon = true;
+                    System.out.printf("\nYou won! Well Done!");
+                    System.out.println("\nThe enemy has been defeated");            
+                    System.out.printf("\nYou have %d\\%d health remaining",Game.pCharacter.getCurrentHealth(),Game.pCharacter.getBaseHealth());              
+                    break;
+                }       
+            } catch (Exception e) {
+                int index = items.Inventory.findItemIndex("Health Potion");
+                if (index != -1) {
+                    System.out.println("Please enter a valid number (1, 2, or 3).");
+                    scanner.nextLine();                           
+                } else {
+                    System.out.println("Please enter a valid number (1, 2).");
+                    scanner.nextLine();             
+                }
+            }
+        }
+    }
+
+    public static void levelUp(GameCharacters player, GameCharacters enemy) {
+        player.setExp(player.getExp() + enemy.getExp());
+        if (player.getExp() >= 100 && player.getLevel() < 5) {
+            player.setLevel(5);
+            player.setBaseHealth(player.getBaseHealth() + 10);  
+            System.out.println("You gained a level!");
+            System.out.println("Your base health went up!");            
+            if (player.getCurrentHealth() <= 10) {
+                player.setCurrentHealth((player.getCurrentHealth() + 1));      
+                System.out.println("Your level up heals you slightly");                                
+            }                    
+        } else if (player.getExp() >= 75 && player.getLevel() < 4) {
+            player.setLevel(4);
+            player.setBaseHealth(player.getBaseHealth() + 10);  
+            System.out.println("You gained a level!");
+            System.out.println("Your base health went up!");            
+            if (player.getCurrentHealth() <= 10) {
+                player.setCurrentHealth((player.getCurrentHealth() + 1));                    
+                System.out.println("Your level up heals you slightly");                  
+            }                    
+        } else if (player.getExp() >= 50 && player.getLevel() < 3) {
+            player.setLevel(3);
+            player.setBaseHealth(player.getBaseHealth() + 10);  
+            System.out.println("You gained a level!");
+            System.out.println("Your base health went up!");            
+            if (player.getCurrentHealth() <= 10) {
+                player.setCurrentHealth((player.getCurrentHealth() + 1));   
+                System.out.println("Your level up heals you slightly");                                   
+            }                    
+        } else if (player.getExp() >= 35 && player.getLevel() < 2) {
+            player.setLevel(2);
+            player.setBaseHealth(player.getBaseHealth() + 10);  
+            System.out.println("You gained a level!");
+            System.out.println("Your base health went up!");            
+            if (player.getCurrentHealth() <= 10) {
+                player.setCurrentHealth((player.getCurrentHealth() + 1));        
+                System.out.println("Your level up heals you slightly");                            
+            }                    
+        } else if (player.getExp() >= 10 && player.getLevel() < 1) {
+            player.setLevel(1);
+            player.setBaseHealth(player.getBaseHealth() + 10);  
+            System.out.println("You gained a level!");
+            System.out.println("Your base health went up!");
+            if (player.getCurrentHealth() <= 10) {
+                player.setCurrentHealth((player.getCurrentHealth() + 1));
+                System.out.println("Your level up heals you slightly");
+            }              
+        } else {
+            System.out.printf("\nYou have %.0f experience!", player.getExp());
+        }
+    }
+
+    public static void lootEnemy(GameCharacters player, GameCharacters enemy) {
+            player.setCoins(player.getCoins() + enemy.getCoins());
+            System.out.printf("\nYou looted %.0f coins.\n", enemy.getCoins());
+            
+            if (enemy.getName().equals("Bandit")) {
+                if (diceRoll.nextBoolean()) {
+                    System.out.println("The enemy dropped a Health Potion!");
+                    items.Inventory.addHealthPotions(1);
+                }
+            } else if (enemy.getName().equals("Dryad")) {
+                if (diceRoll.nextBoolean()) {
+                    System.out.println("The enemy dropped a Health Potion!");
+                    items.Inventory.addHealthPotions(1);
+                }                
+            } else if (enemy.getName().equals("Shreik")) {
+                System.out.println("The enemy dropped a Health Potion!");
+                items.Inventory.addHealthPotions(2);
+            } else if (enemy.getName().equals("Orc") || enemy.getName().equals("Goblin") || enemy.getName().equals("Wight") || enemy.getName().equals("Wraith"))  {
+                System.out.println("The enemy dropped a Health Potion!");
+                items.Inventory.addHealthPotions(2);                
+            } else if (enemy.getName().equals("Balrog") || enemy.getName().equals("Frost Dragon"))  {
+               System.out.println("The enemy dropped a Health Potion!");
+                items.Inventory.addHealthPotions(4);                
+            }
+        }
+}
